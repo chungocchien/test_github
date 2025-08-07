596b8504d25ff67754a02068294a9363a80a0cf0
just chien <82660465+chungocchien@users.noreply.github.com>
2025-08-07 15:23:17 +0700

    Delete .github/workflows/code_2.py

--- .github/workflows/code_2.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-df = pd.DataFrame({
-    'month': ['Jan', 'Feb', 'Mar', 'Apr', 'May'],
-    'sales': [200, 220, 250, 275, 300],
-    'profit': [50, 60, 70, 80, 90]
-})
-print(df.describe())
